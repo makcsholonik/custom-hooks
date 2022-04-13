c9cfec98d8cb13cbe5fe3de9a5693aa68adb872e
makcsholonik <makcsholonik@gmail.com>
2022-04-13 13:56:57 +0300

    useInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useInput } from './hooks/useInput';
 
 export function App () {
+
+	const userName = useInput ( '' );
+	const password = useInput ( '' );
+
 	return (
-		<div className="App">
-			Рaботает!
+		<div>
+			<input { ...userName } type={ 'text' } placeholder={ 'username' }/>
+			<input  { ...password } type={ 'text' } placeholder={ 'password' }/>
+			<button onClick={ () => console.log ( userName.value, password.value ) }>click</button>
 		</div>
 	);
 }
